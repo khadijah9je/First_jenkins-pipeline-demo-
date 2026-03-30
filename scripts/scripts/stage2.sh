23f6f725ac066846e5d0dd6783801d872ab64bc0
khadijah9je <160172966+khadijah9je@users.noreply.github.com>
2026-03-30 15:05:50 +0200

    Delete scripts/scripts directory

--- scripts/scripts/stage2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "============================================"
-echo "  Hello from Stage 2 Shell Script!"
-echo "  Build Number: $BUILD_NUMBER"
-echo "  Workspace: $WORKSPACE"
-echo "============================================"
